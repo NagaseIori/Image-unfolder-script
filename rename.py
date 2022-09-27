ed753f400fe180880c0a16a4e17efad92c03dbdf
NordLandeW <31349569+NagaseIori@users.noreply.github.com>
2022-09-27 20:32:07 +0800

    Update rename.py

--- rename.py
+++ rename.py
@@ -5,7 +5,7 @@ def fun(dir):
     entries = os.scandir(dir)
     for entry in entries:
         if entry.is_file():
-            new_name = entry.path[2:].replace("\\", "_").replace("/", "_").replace(" ", "_")
+            new_name = entry.path[2:].replace("_", "__").replace("\\", "_").replace("/", "_")
             os.rename(entry.path, "./"+new_name)
         else:
             fun(dir+"/"+entry.name)
