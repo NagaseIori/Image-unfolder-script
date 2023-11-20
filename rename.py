d113f056bdf35c6fec178b1a63bcf27383f488e1
Rin <31349569+NagaseIori@users.noreply.github.com>
2023-11-20 12:17:44 +0800

    better

--- rename.py
+++ rename.py
@@ -1,26 +1,46 @@
 import os
 import shutil
+import json
 from sys import argv
+
+library = None
 
 def fun(dir, head, floor):
     entries = os.scandir(dir)
     for entry in entries:
         if entry.is_file():
-            new_name = entry.path[:head]+entry.path[head:].replace("_", "-U").replace("\\", "_").replace("/", "_")
-            print("Rename %s to %s."%(entry.path, new_name))
-            if not os.path.exists(new_name):
-                if entry.path.find("-U") != -1:
-                    print("File %s has been renamed. Skipped."%(entry.path))
+            new_name = entry.path[:head]+entry.path[head:].replace("\\", "_").replace("/", "_")
+            if new_name != entry.path:
+                if not os.path.exists(new_name):
+                    if entry.path in library:
+                        print("File %s has been renamed. Skipped."%(entry.path))
+                    else:
+                        print("Rename %s to %s."%(entry.path, new_name))
+                        os.rename(entry.path, new_name)
+                        library[new_name] = entry.path
                 else:
-                    os.rename(entry.path, new_name)
-            else:
-                print("File %s has existed. Skipped." % new_name)
+                    print("File %s has existed. Skipped." % new_name)
         else:
-            fun(dir+"/"+entry.name, head+(len(entry.name)+1 if floor>0 else 0), floor-1)
+            fun(dir+"\\"+entry.name, head+(len(entry.name)+1 if floor>0 else 0), floor-1)
             if floor<=0:
                 os.rmdir(entry.path)
 
+try:
+    with open(".rename_lib", "r") as f:
+        library = json.load(f)
+        f.close()
+    print("Found rename library.")
+except:
+    print("Create a new rename library.")
+    library = dict()
+
 floor = int(input("floor:"))
 fun(".", 2, floor)
+
+with open(".rename_lib", "w") as f:
+    json.dump(library, f)
+    f.close()
+
 input("Press any key to exit.")
+
 os.remove(argv[0])
--- repack.py
+++ repack.py
@@ -1,19 +1,38 @@
 from genericpath import exists
 import os
+import json
 import shutil
+from sys import argv
 
 entries = os.scandir(".")
+library = None
+
+try:
+    with open(".rename_lib", "r") as f:
+        library = json.load(f)
+        f.close()
+    print("Found rename library.")
+    print(library)
+except:
+    library = None
+    print("Library not found. Fallback to classic methods.")
 
 for entry in entries:
     if entry.is_file():
         new_path = entry.path
-        nlist = list(new_path)
-        if(new_path.rfind("_") == -1 and new_path.rfind("-U") == -1):
-            continue
-        if new_path.rfind("_") !=-1:
-            nlist[new_path.rfind("_")] = '\\'
-        new_path = ''.join(nlist) ;
-        new_path = new_path.replace("_", "/").replace("-U", "_");
+        if library == None:
+            nlist = list(new_path)
+            if(new_path.rfind("_") == -1 and new_path.rfind("-U") == -1):
+                continue
+            if new_path.rfind("_") !=-1:
+                nlist[new_path.rfind("_")] = '\\'
+            new_path = ''.join(nlist) ;
+            new_path = new_path.replace("_", "/").replace("-U", "_");
+        else:
+            if entry.path in library:
+                new_path = library[entry.path]
+            else:
+                print(f"{entry.path} not in library file. Skip.")
         # print(new_path[0:new_path.rfind("\\")]);
         print("Rename %s to %s" % (entry.path, new_path))
         if entry.path.rfind("_")!=-1:
@@ -22,7 +41,11 @@ for entry in entries:
         if not os.path.exists(new_path):
             os.rename(entry.path, new_path)
         else:
-            print("File %s has existed. Skipped." % new_path)
+            print("File %s has existed. Skip." % new_path)
             # os.remove(entry.path)
 
+print("Press anykey to continue.")
 input()
+
+os.remove(".rename_lib")
+os.remove(argv[0])
